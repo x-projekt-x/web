f890313fb82c36c9591b40e1fdb45c033fe4a479
creator110 <creator110@gmx.de>
2015-05-07 13:35:10 +0200

    Create candc.php

---
+++ css/candc.php
@@ -0,0 +1,40 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/main.css" type="text/css" /> 
+	</head>
+	
+	<div id="header">
+		CandC - Brogrammers
+		<div id="header2">
+			Webhosting, Webdesign, Gameserver
+		</div>
+	</div>
+	<hr>
+	<div id="navlist">
+		<li>|<a href="#"> Home </a>|</li>
+		<li><a href="#">Teamspeak Server </a>|</li>
+		<li><a href="#">Webhosting </a>|</li>
+		<li><a href="#">Webdesign </a>|</li>
+		<li><a href="#">Impressum </a>|</li>
+		<li><a href="#">Login </a>|</li>
+				
+	</div>
+	<hr>
+	<div id="sidebar">
+		<li>bla1</li>
+		<li>bla2</li>
+		<li>bla3</li>
+	</div>
+	<div id="body">
+		
+		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor <br />
+		invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et <br />
+		justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.<br />
+		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore<br />
+		magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, <br />
+		no sea takimata sanctus est Lorem ipsum dolor sit amet.<br />
+	</div>
+	
+	<div id="footer">
+		<img src="logo.png" alt="logo">
+	</div>
