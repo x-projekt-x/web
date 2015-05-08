d7a520d4aeb7180d8fc49a9cd01c6d0f23443a0d
creator110 <creator110@gmx.de>
2015-05-08 08:46:56 +0200

    Update login.php

--- css/navlist/login.php
+++ css/navlist/login.php
@@ -1,1 +1,95 @@
-Login
+<h2>Login</h2>
+<?php
+
+	/**
+	 * Fuer das Anmelden muss der Benutzer 
+	 * 
+	 * 	a) sich identifizieren
+	 * 	b) beweisen, dass er es wirklich ist
+	 * 
+	 * Die eigegeben Anmeldung (credentials) werden gegen
+	 * 
+	 */
+	 
+	if(isset($_POST['anmelden'])) {
+		
+		$link = mysql_connect('host' , 'user', 'password')
+			or die(mysql_error());
+		
+		mysql_select_db('db_name')
+			or die(mysql_error());
+			
+		$email = $_POST['email'];
+		$pass = md5($_POST['pass']);
+		
+		$query = 	"SELECT
+						*
+					 FROM
+						db_table
+					 WHERE
+						email='".$email."'
+					 AND
+					 	pass='".$pass."'
+					 		";
+		
+		$result = mysql_query($query);
+		
+		// falls genau ein Datensatz abgefragt werden konnte ...
+		if(mysql_num_rows($result) ==1) {
+			
+			// ... sind wir "drin"
+			
+			// ... und wir speichern die erfolgreiche Anmeldung und 
+			// den Benutzernamen (hier Email) in der Sitzung 
+			$_SESSION['login'] 	= true;
+			$_SESSION['user']	=$email;
+                        
+		}
+		else{
+	
+			// ... sonst sind wir "draussen"
+			echo "Pruefen Sie die Anmeldedaten";
+	}
+	}
+	
+	// falls sich jemand abmelden will...
+	if(isset($_POST['abmelden'])){
+		
+		// ...loeschen wir die Sitzungsdaten
+		$_SESSION = array();
+		 
+	}
+	
+	//das Anmeldeformular anzeigen, falls der Benutzer noch nicht angemeldet ist
+	if($_SESSION['login'] != true) {
+?>
+
+	<form action="" method="post">
+	
+		<label for="email">E-Mail:</label>
+		<br />
+		<input type="text" name="email" id="email" />
+		<br />
+		<br />
+
+		<label for="pass">Passwort:</label>
+		<br />
+		<input type="password" name="pass" id="pass" />
+		<br />
+		<br />
+		
+		<input type="submit" name="anmelden" id="anmelden" value="Login" />
+	
+	</form>
+<?php 
+	}
+	else {
+?>
+	 <form action="" method="post">
+	 	<button name="abmelden" id="abmelden">
+	 		Logout
+	 	</button>
+	 </form>
+<?php 
+	 }
+?>
